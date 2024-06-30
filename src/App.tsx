b81585c2dc1bc488b6854c0082a5ef297eef7ad1
Orian Georgian <orian.georgian.external@fujitsu.com>
2024-06-30 12:23:23 +0300

    (feat) Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header/Header";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Header</header>
+      <Header />
     </div>
   );
 }
